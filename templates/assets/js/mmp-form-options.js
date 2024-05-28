3a9f4d32b687a516536d8e1ea4afd7d380263da3
Rob Moore <rob.moore@memberminderpro.com>
2024-05-28 11:15:31 -0500

    Pre-release cleanup

--- templates/assets/js/mmp-form-options.js
+++ templates/assets/js/mmp-form-options.js
@@ -3,8 +3,9 @@
 const mmpFormOptions = {
     recaptcha_site_key: "6LfUu-UpAAAAAJy58Tuq2oZxnlTxV5bgtcCWigzP",
     recaptcha_secret_key: "6LfUu-UpAAAAAFL7KXvU05wtrksT7tdOVe1CvUTm",
-    account_ID: "10651",
-    BID: "1065",
+    account_ID: 10651,
+    BID: 1065,
+    ClubID: 989306,
     account_email: "wash-rag@rmoore.dev",
     default_member_type_id: 938,
     membership_cost: 25,
