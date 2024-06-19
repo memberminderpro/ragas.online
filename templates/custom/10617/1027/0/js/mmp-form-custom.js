54ee68be68c1f4ae9ef48fe5cecf937be6487a3e
Rob Moore <rob.moore@memberminderpro.com>
2024-06-19 17:20:19 -0500

    Scaffolding the custom form for Eagle River Yacht Club

--- templates/custom/10617/1027/0/js/mmp-form-custom.js
+++ templates/custom/10617/1027/0/js/mmp-form-custom.js
@@ -1,178 +1,110 @@
 $(document).ready(function () {
+  if (typeof mmpFormOptions !== 'undefined') {
+    var urlParams = new URLSearchParams(window.location.search);
+    var $mtid = urlParams.get('mtid');
 
-  // Functionality to handle showing/hiding the Co-Member information section
-  const coMemberCheckbox = document.getElementById("CoMember");
-  if (coMemberCheckbox) {
-    coMemberCheckbox.addEventListener("change", function () {
-      const coMemberDiv = document.getElementById("CoMemberDiv");
-      if (coMemberCheckbox.checked) {
-        coMemberDiv.style.display = "block";
-      } else {
-        coMemberDiv.style.display = "none";
+    if ($mtid) {
+      $mtid = parseInt($mtid, 10);
+    } else {
+      $mtid = parseInt(mmpFormOptions.default_member_type_id, 10);
+    }
+
+    // Set the initial value of MCY based on $mtid
+    switch ($mtid) {
+      case 933:
+        $("#mcy_933").prop("checked", true);
+        break;
+      case 935:
+        $("#mcy_935").prop("checked", true);
+        break;
+      case 938:
+      default:
+        $("#mcy_938").prop("checked", true);
+        break;
+    }
+
+    // Update the $mtid variable whenever the selection changes
+    $("input[name='MCY']").change(function () {
+      var selectedValue = $(this).val();
+      $mtid = parseInt(selectedValue.split('|')[0], 10);
+      console.log("current mtid: " + $mtid);
+    });
+
+    // Handle changes to club type and update relevant fields
+    $("#fkclubtype").change(function () {
+      var mt = $(this).val();
+      switch (mt) {
+        case "Rotary Club":
+          $('input[name="MemberCategoryIDs"][value="390"]').prop("checked", true);
+          $('input[name="MemberCategoryIDs"][value="388"]').prop("checked", false);
+          break;
+        case "Rotaract Club":
+          $('input[name="MemberCategoryIDs"][value="388"]').prop("checked", true);
+          $('input[name="MemberCategoryIDs"][value="390"]').prop("checked", false);
+          break;
+        case "Non-Rotarian":
+          $('input[name="MemberCategoryIDs"][value="388"]').prop("checked", false);
+          $('input[name="MemberCategoryIDs"][value="390"]').prop("checked", false);
+          break;
       }
     });
   }
 
-  const coMemberDiv = document.getElementById("CoMemberDiv");
-  if (coMemberDiv) {
-    coMemberDiv.style.display = "none";
-  }
+  // Additional form validation logic
+  const form = document.querySelector('form');
+  const MemberCategoryIDs = document.querySelectorAll('input[name="MemberCategoryIDs"]');
+  const interestsCheckboxes = document.querySelectorAll('#interests input[name="MemberCategoryIDs"]');
+  const fkclubtype = document.getElementById('fkclubtype');
+  const clubname = document.getElementById('clubname');
+  const clubNameError = document.getElementById('ClubLocDiv');
 
-  function updateBirthDate(containerId, monthSelector, daySelector, hiddenFieldSelector) {
-    var month = $(containerId).find(monthSelector).val();
-    var day = $(containerId).find(daySelector).val();
-    if (month && day) {
-      $(containerId).find(hiddenFieldSelector).val(month + '/' + day + '/1896');
-    }
-  }
+  form.addEventListener('submit', (event) => {
+    let mcidValues = [];
+    let isExperienceSelected = false;
+    let isInterestSelected = false;
 
-  $("#birthday_fields #BirthMon, #birthday_fields #BirthDay").on("change", function () {
-    updateBirthDate('#birthday_fields', '#BirthMon', '#BirthDay', '#BirthDate');
-  });
-
-  $("#partner_birthdate_fields #PartnerBirthMon, #partner_birthdate_fields #PartnerBirthDay").on("change", function () {
-    updateBirthDate('#partner_birthdate_fields', '#PartnerBirthMon', '#PartnerBirthDay', '#PartnerBirthDate');
-  });
-
-  $(".BoatType input").click(function () {
-    if (this.value != "None") {
-      $(".BoatDiv").show();
-      $("#UDF82").prop("required", true);
-    } else {
-      $(".BoatDiv").hide();
-      $("#UDF82").prop("required", false);
-    }
-  });
-
-  $("#Consent").click(function () {
-        if (debug == true) {
-            console.log("consent");
-        }
-    if ($(this).prop("checked")) {
-      $("#Send").show();
-      if ($("#signature").val() == "") {
-        $("#signature").val(
-          $("#FirstName").val() +
-            " " +
-            $("#MidName").val() +
-            " " +
-            $("#LastName").val()
-        );
-        $("#ipaddress").val(myIP());
-        const d = new Date();
-        $("#esaigdate").val(
-          d.getMonth() +
-            1 +
-            "/" +
-            d.getDate() +
-            "/" +
-            d.getFullYear() +
-            " " +
-            d.getHours() +
-            ":" +
-            d.getMinutes()
-        );
+    MemberCategoryIDs.forEach((field) => {
+      if (field.type === 'radio' && field.checked) {
+        isExperienceSelected = true;
       }
-    } else {
-      $("#Send").hide();
+      if (field.checked) {
+        mcidValues.push(field.value);
+      }
+    });
+
+    interestsCheckboxes.forEach((checkbox) => {
+      if (checkbox.checked) {
+        isInterestSelected = true;
+      }
+    });
+
+    if (!isExperienceSelected) {
+      alert('Please select your WASH Experience.');
+      event.preventDefault();
+      return;
     }
+
+    if (!isInterestSelected) {
+      alert('Please select at least one area of interest.');
+      event.preventDefault();
+      return;
+    }
+
+    const mcidString = mcidValues.join(', ');
+    const mcidHiddenInput = document.createElement('input');
+    mcidHiddenInput.type = 'hidden';
+    mcidHiddenInput.name = 'mcid';
+    mcidHiddenInput.value = mcidString;
+    form.appendChild(mcidHiddenInput);
   });
 
-  $("#prtapp").click(function () {
-    if (debug == true) {
-    console.log("print");
-    }
-    $("#PrintContent").printThis();
-    return false;
-  });
-
-  function validateBoatFields() {
-    let isValid = true;
-    const length = document.getElementById("UDF85");
-    const beam = document.getElementById("UDF106");
-    const draft = document.getElementById("UDF107");
-
-    if (length.value < 1 || length.value > 100) {
-      $("#LengthError").show();
-      isValid = false;
+  fkclubtype.addEventListener('change', function () {
+    if (this.value === 'Rotary Club' || this.value === 'Rotaract Club') {
+      clubname.required = true;
+      clubNameError.textContent = 'Please select your club name.';
     } else {
-      $("#LengthError").hide();
-    }
-
-    if (beam.value < 1 || beam.value > 30) {
-      $("#BeamError").show();
-      isValid = false;
-    } else {
-      $("#BeamError").hide();
-    }
-
-    if (draft.value < 1 || draft.value > 20) {
-      $("#DraftError").show();
-      isValid = false;
-    } else {
-      $("#DraftError").hide();
-    }
-
-    return isValid;
-  }
-
-  function validateUDF83() {
-    let isValid = true;
-    const udf83Selected = document.querySelector('input[name="UDF83"]:checked');
-    if (!udf83Selected) {
-      $('input[name="UDF83"]').addClass("invalid-radio");
-      if (debug == true) {
-      console.log("Invalid UDF83 selection (Boat Type)");
-      }
-      isValid = false;
-    } else {
-      $('input[name="UDF83"]').removeClass("invalid-radio");
-      if (udf83Selected.value !== "None" && !$("#UDF82").val()) {
-        $("#UDF82").addClass("invalid-field");
-        $("#UDF82Error").show();
-        isValid = false;
-      } else {
-        $("#UDF82").removeClass("invalid-field");
-        $("#UDF82Error").hide();
-      }
-    }
-    return isValid;
-  }
-
-  function validateForm() {
-    const isFormValid = validateBoatFields() && validateUDF83();
-    return isFormValid;
-  }
-
-  $("#mmp-form").on("submit", function (event) {
-    if (!validateForm()) {
-      event.preventDefault(); // Prevent form submission if validation fails
-      alert("Please complete the required fields.");
+      clubname.required = false;
+      clubNameError.textContent = '';
     }
   });
 });
-
-// Function to get user's IP address
-function myIP() {
-  if (window.XMLHttpRequest) xmlhttp = new XMLHttpRequest();
-  else xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-
-  xmlhttp.open("GET", "https://api.hostip.info/get_html.php", false);
-  xmlhttp.send();
-
-  hostipInfo = xmlhttp.responseText.split("\n");
-
-  for (i = 0; hostipInfo.length >= i; i++) {
-    ipAddress = hostipInfo[i].split(":");
-    if (ipAddress[0] == "IP") return ipAddress[1];
-  }
-  return false;
-}
-
-// Function to prevent form submission on Enter key press
-function noEnter(e) {
-  var keycode;
-  if (window.event) keycode = window.event.keyCode;
-  else if (e) keycode = e.which;
-  return !(keycode == 13);
-}
